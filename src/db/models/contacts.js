588be3bd6757e39a881d742171316e2c361d3105
iyrd <chilromko@gmail.com>
2025-07-19 14:13:09 +0200

    done

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -1,27 +1,21 @@
 import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  phoneNumber: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String
-  },
-  isFavourite: {
-    type: Boolean,
-    default: false
-  },
+  name: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  email: { type: String },
+  isFavourite: { type: Boolean, default: false },
   contactType: {
     type: String,
     enum: ['work', 'home', 'personal'],
     required: true,
-    default: 'personal'
-  }
+    default: 'personal',
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 }, { timestamps: true });
 
 export const Contact = mongoose.model('Contact', contactSchema);
